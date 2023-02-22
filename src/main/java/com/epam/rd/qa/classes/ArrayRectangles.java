8e19c161c3d921b8133ebeb136617c333d037e7f
Olha Mushket <olhamushket@gmail.com>
2023-02-22 04:27:39 +0200

    Solved

--- src/main/java/com/epam/rd/qa/classes/ArrayRectangles.java
+++ src/main/java/com/epam/rd/qa/classes/ArrayRectangles.java
@@ -1,39 +1,63 @@
 package com.epam.rd.qa.classes;
 
 public class ArrayRectangles {
+    private final Rectangle[] rectangleArray;
 
-    public ArrayRectangles(int size) {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+    public ArrayRectangles(int size) throws IllegalArgumentException{
+        if (size<=0) {
+            throw new IllegalArgumentException();
+        }
+        this.rectangleArray = new Rectangle[size];
     }
 
-    public ArrayRectangles(Rectangle... rectangles) {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+    public ArrayRectangles(Rectangle... rectangles) throws IllegalArgumentException{
+        if (rectangles==null||rectangles.length==0) {
+            throw new IllegalArgumentException();
+        }
+        this.rectangleArray = rectangles;
     }
 
     public boolean addRectangle(Rectangle rectangle) {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < rectangleArray.length; i++) {
+            if (rectangleArray[i]==null) {
+                rectangleArray[i]=rectangle;
+                return true;
+            }
+        }
+        return false;
+
     }
 
     public int size() {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+        int amount=0;
+        for (Rectangle rectangle : rectangleArray) {
+            if (rectangle != null) amount++;
+        }
+        return amount;
     }
 
     public int indexMaxArea() {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+        int indexMax=0;
+        for (int i = 0; i < size(); i++) {
+            if (rectangleArray[indexMax].area()<rectangleArray[i].area()) indexMax=i;
+
+        }
+        return indexMax;
     }
 
     public int indexMinPerimeter() {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+        int indexMin=0;
+        for (int i = 0; i < size(); i++) {
+            if (rectangleArray[indexMin].perimeter() > rectangleArray[i].perimeter()) indexMin = i;
+        }
+        return indexMin;
     }
 
     public int numberSquares() {
-        // TODO place your code here
-        throw new UnsupportedOperationException();
+        int square=0;
+        for (int i = 0; i < size(); i++) {
+            if (rectangleArray[i].isSquare()) square++;
+        }
+        return square;
     }
 }
--- src/main/java/com/epam/rd/qa/classes/Rectangle.java
+++ src/main/java/com/epam/rd/qa/classes/Rectangle.java
@@ -1,4 +1,76 @@
 package com.epam.rd.qa.classes;
 
+import java.util.Objects;
+
 public class Rectangle {
+    private double sideA, sideB;
+
+    public Rectangle(double sideA, double sideB) throws IllegalArgumentException{
+        if (sideA<=0||sideB<=0) {
+            throw new IllegalArgumentException();
+        }
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public Rectangle(double sideA) throws IllegalArgumentException{
+        if (sideA<=0) {
+            throw new IllegalArgumentException();
+        }
+        this.sideA = sideA;
+        this.sideB = sideA;
+    }
+
+    public Rectangle() {
+        this.sideA = 4.0;
+        this.sideB = 3.0;
+    }
+
+    public double getSideA() {
+        return sideA;
+    }
+
+    public double getSideB() {
+        return sideB;
+    }
+
+    public double area() {
+        return sideA*sideB;
+    }
+
+    public double perimeter() {
+        return (sideA+sideB)*2;
+    }
+
+    public boolean isSquare() {
+        return sideA==sideB;
+    }
+
+    public void replaceSides () {
+        double rep;
+        rep =sideA;
+        sideA=sideB;
+        sideB=rep;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Rectangle)) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return Double.compare(rectangle.getSideA(), getSideA()) == 0 && Double.compare(rectangle.getSideB(), getSideB()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getSideA(), getSideB());
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle{" +
+                "sideA=" + sideA +
+                ", sideB=" + sideB +
+                '}';
+    }
 }
